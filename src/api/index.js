493919ab2643fb4efc5c0c3e1083b1ca831bdc65
gonzalezmassini <gonzalez.massini@gmail.com>
2021-12-07 00:01:54 -0400

    change connection to heroku

--- src/api/index.js
+++ src/api/index.js
@@ -15,7 +15,7 @@ import axios from "axios"
 //   })
 
 // const localhost = 'http://127.0.0.1:5000/gelatok'
-const localhost = true
+const localhost = false
 const url = localhost ? 'http://127.0.0.1:5000/gelatok' : 'https://bdproject-restapi.herokuapp.com/gelatok'
 
 
